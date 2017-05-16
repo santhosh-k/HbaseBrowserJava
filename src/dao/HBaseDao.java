8f371d6c5bb9fde9b9572fdbd2918c4a26526db9
Santhosh <santhosh.k@aol.com>
2017-05-16 23:23:02 +0530

    Added UI Files

--- src/dao/HBaseDao.java
+++ src/dao/HBaseDao.java
@@ -2,33 +2,111 @@ package dao;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.hbase.HBaseConfiguration;
-import org.apache.hadoop.hbase.MasterNotRunningException;
-import org.apache.hadoop.hbase.TableName;
-import org.apache.hadoop.hbase.ZooKeeperConnectionException;
+import org.apache.hadoop.hbase.HTableDescriptor;
 import org.apache.hadoop.hbase.client.HBaseAdmin;
+import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.codehaus.jackson.map.ObjectMapper;
+
+import commons.CommonUtils;
+import commons.Constants;
 
 public class HBaseDao {
-	public static final String HBASE_CONFIGURATION_ZOOKEEPER_QUORUM = "hbase.zookeeper.quorum";
-	public static final String HBASE_CONFIGURATION_ZOOKEEPER_CLIENTPORT = "hbase.zookeeper.property.clientPort";
-	HBaseAdmin admin = null;
-	public HBaseDao() throws MasterNotRunningException, ZooKeeperConnectionException, IOException {
-		Configuration hConf = HBaseConfiguration.create();
-		hConf.set(HBASE_CONFIGURATION_ZOOKEEPER_QUORUM, "127.0.0.1");
-		hConf.setInt(HBASE_CONFIGURATION_ZOOKEEPER_CLIENTPORT, 2181);
-		admin = new HBaseAdmin(hConf);
-	}
+	
+	public Map<String , Object> getHbaseTables(HBaseAdmin admin) throws IOException {
+		Map<String , Object> tableMap = new HashMap<>();
+		
+		List<String> tableList = new ArrayList<String>();
 
-	public List<String> getTables() throws IOException{
-		List<String> response = new ArrayList<String>();
-		TableName[] tables = admin.listTableNames();
-		for (TableName tableName : tables) {
-			response.add(tableName.getNameAsString());
+		// Getting all the list of tables using HBaseAdmin object
+		HTableDescriptor[] htabelList = admin.listTables();
+
+		// printing all the table names.
+		for (HTableDescriptor htableDescriptor : htabelList) {
+			String hTableName = htableDescriptor.getNameAsString();
+			if (hTableName.contains("_")) {
+				continue;
+			}
+			tableList.add(hTableName);
 		}
-		return response;
+		
+		tableMap.put(Constants.DATA,tableList);
+		
+		return tableMap;
 	}
-
+	
+	/**
+	 * Adds the row.
+	 *
+	 * @param uniqueRowKey
+	 *            the unique row key
+	 * @param data
+	 *            the data
+	 * @param fieldsToIgnore
+	 *            the fields to ignore
+	 * @throws IOException 
+	 */
+	public void addRow(String uniqueRowKey, Object data,
+			List<String> fieldsToIgnore, String columnFamily,String tableName , HBaseAdmin admin)
+			throws IOException {
+		Put put = generatePut(uniqueRowKey, columnFamily, data, fieldsToIgnore,tableName);
+		addRow(put,admin,tableName);
+	}
+	
+	/**
+	 * Generate put.
+	 *
+	 * @param uniqueRowKey
+	 *            the unique row key
+	 * @param data
+	 *            the data
+	 * @param fieldsToIgnore
+	 *            the fields to ignore
+	 * @return the put
+	 */
+	@SuppressWarnings({ "deprecation", "unchecked" })
+	public Put generatePut(String uniqueRowKey, String columnFamily,
+			Object data, List<String> fieldsToIgnore,String tableName) {
+		ObjectMapper objectMapper = new ObjectMapper();
+		Map<String, Object> dataMap = objectMapper.convertValue(data,
+				HashMap.class);
+		Put put = new Put(Bytes.toBytes(uniqueRowKey));
+		for (Map.Entry<String, Object> entry : dataMap.entrySet()) {
+			String key = entry.getKey();
+			if (fieldsToIgnore != null && fieldsToIgnore.contains(key)) {
+				continue;
+			}
+			Object value = entry.getValue();
+			if (value == null) {
+				value = "";
+			}
+			if (columnFamily != null) {
+				columnFamily = tableName;
+			}
+			put.add(Bytes.toBytes(columnFamily), Bytes.toBytes(key),
+					CommonUtils.convertIntoBytes(value));
+		}
+		return put;
+	}
+	
+	
+	/**
+	 * Adds the row.
+	 *
+	 * @param put
+	 *            the put
+	 * @throws IOException 
+	 */
+	@SuppressWarnings("deprecation")
+	public void addRow(Put put,HBaseAdmin hbaseAdmin,String tableName) throws IOException {
+		HTable hTable = new HTable(hbaseAdmin.getConfiguration(), tableName);
+		hTable.put(put);
+		hTable.close();
+	}
+	
 }
--- src/main/UserService.java
+++ src/main/UserService.java
@@ -1,28 +1,36 @@
 package main;
 
 import java.io.IOException;
+import java.math.BigInteger;
+import java.security.SecureRandom;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.ws.rs.CookieParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Cookie;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-
-import net.minidev.json.JSONObject;
-import net.minidev.json.JSONValue;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.MasterNotRunningException;
+import org.apache.hadoop.hbase.ZooKeeperConnectionException;
 import org.apache.hadoop.hbase.client.HBaseAdmin;
 
 import commons.CommonUtils;
 import commons.Constants;
+import dao.HBaseDao;
+import net.minidev.json.JSONObject;
+import net.minidev.json.JSONValue;
 
 @Path("/hbaseService")
 public class UserService {
-		
+
 	/**
 	 * CONFIGURATION holds the unique configuration of hbase with its individual
 	 * paramters in it.
@@ -35,18 +43,29 @@ public class UserService {
 	 */
 	private static HBaseAdmin admin = null;
 
+	/**
+	 * The static map that holds the session.
+	 */
+
 	private static Map<String, Map<String, String>> sessionMap = new HashMap<String, Map<String, String>>();
 
+	/**
+	 * The service that register the user who enters into the application.
+	 * 
+	 * @param connectionJson
+	 * @return
+	 */
 	@POST
 	@Path("/connect")
-	public Response connectToHbase(String connectionJson) {
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response connectToHbase(
+			@HeaderParam(value = "sessionId") String sessionId,
+			String connectionJson) {
 
 		Response response = null;
 
 		Map<String, Object> dataMap = new HashMap<>();
-		String sessionId = "sess";
 		try {
-			// String sessionId = request.getSession().getId();
 			JSONObject connectionObject = (JSONObject) JSONValue
 					.parse(connectionJson);
 
@@ -56,24 +75,33 @@ public class UserService {
 
 			String apiKey = (String) data.get(Constants.API_KEY);
 
-			if (sessionMap.containsKey(sessionId)) {
-				Map<String, String> valueMap = sessionMap.get(sessionId);
-				apiKey = valueMap.get(Constants.API_KEY);
-				ipAddress = valueMap.get(Constants.IPADDRESS);
+			Map<String, String> valueMap = new HashMap<String, String>();
+
+			if (sessionId != null) {
+				if (sessionMap.containsKey(sessionId)) {
+					valueMap = sessionMap.get(sessionId);
+					apiKey = valueMap.get(Constants.API_KEY);
+					ipAddress = valueMap.get(Constants.IPADDRESS);
+				} else {
+					valueMap.put(Constants.IPADDRESS, ipAddress);
+					valueMap.put(Constants.API_KEY, apiKey);
+					sessionMap.put(sessionId, valueMap);
+					admin = getHbaseConnection(ipAddress);
+
+				}
 			} else {
-				Map<String, String> valueMap = new HashMap<String, String>();
+				sessionId = next();
 				valueMap.put(Constants.IPADDRESS, ipAddress);
 				valueMap.put(Constants.API_KEY, apiKey);
 				sessionMap.put(sessionId, valueMap);
 				admin = getHbaseConnection(ipAddress);
-
+				dataMap.put(Constants.SESSIONID, sessionId);
 			}
 
 			if (admin != null) {
 				Map<String, String> successMap = new HashMap<String, String>();
 				successMap.put(Constants.DATA, Constants.CONNECTED);
 				dataMap.put(Constants.SUCCESS, successMap);
-
 				response = CommonUtils.buildSuccessResponse(dataMap);
 
 			} else {
@@ -91,6 +119,11 @@ public class UserService {
 
 	}
 
+	/**
+	 * 
+	 * @param ipAddress
+	 * @return
+	 */
 	public static HBaseAdmin getHbaseConnection(String ipAddress) {
 		try {
 			if (admin == null) {
@@ -110,29 +143,30 @@ public class UserService {
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
-			return null ;
+			return null;
 		}
 		return admin;
 	}
 
 	@GET
 	@Path("/getTables")
-	public Response getHtables(@QueryParam(Constants.SESSIONID) String sessionId) {
+	public Response getHtables(@CookieParam("sessionId") Cookie cookie) {
 
 		Response response = null;
-		
-		Map<String, String> valueMap = sessionMap.get(sessionId);
-		if(valueMap == null){
-			response = CommonUtils.buildFailureResponse("Please connect to hbase first");
+
+		Map<String, String> valueMap = sessionMap.get(cookie.getValue());
+		if (valueMap == null) {
+			response = CommonUtils
+					.buildFailureResponse("Please connect to hbase first");
 			return response;
 		}
 		String ipAddress = valueMap.get(Constants.IPADDRESS);
-		HBaseAdmin	hbaseAdmin = getHbaseConnection(ipAddress);
+		HBaseAdmin hbaseAdmin = getHbaseConnection(ipAddress);
 
-		HbaseHelper hbaseHelper = getHhbaseHelper();
 
 		try {
-			Map<String, Object> tableMap = hbaseHelper.getHbaseTables(hbaseAdmin);
+			Map<String, Object> tableMap = getHbaseDao()
+					.getHbaseTables(hbaseAdmin);
 			response = CommonUtils.buildSuccessResponse(tableMap);
 			return response;
 
@@ -143,8 +177,31 @@ public class UserService {
 		return response;
 
 	}
+	
+	
+	
 
 	private HbaseHelper getHhbaseHelper() {
 		return new HbaseHelper();
+	}
+	
+	private HBaseDao getHbaseDao() throws MasterNotRunningException, ZooKeeperConnectionException, IOException{
+		return new HBaseDao();
+	}
+
+	@POST
+	@Path("/disconnect")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response logout(@HeaderParam(value = "sessionId") String sessionId) {
+		Response response = CommonUtils.buildSuccessResponse("Logged out successfully");
+		sessionMap.remove(sessionId);
+		
+		return response;
+
+	}
+
+	public String next() {
+		SecureRandom ran = new SecureRandom();
+		return new BigInteger(130, ran).toString(32);
 	}
 }
